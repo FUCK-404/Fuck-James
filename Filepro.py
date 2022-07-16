8d544eb144ac978c84ea9b8a5a610f9b6a50e616
RINKU-CYBER-404 <109244698+RINKU-CYBER-404@users.noreply.github.com>
2022-07-16 18:44:52 +0600

    Update Filepro.py

--- Filepro.py
+++ Filepro.py
@@ -1,9 +1,15 @@
 import platform
-
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch .prox.txt')
+except:pass
+try:os.system('touch .proxy.txt')
+except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Fipp32")._site_view_()
+	__import__("latter")._site_view_()
 elif 'aarch' in arc:
-	__import__("Fippx")._site_view_()
+	__import__("Filpp").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
